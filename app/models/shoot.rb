b821669717fe85f683662956e35fc3c78912e106
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-02-23 19:00:38 -0600

    added associations

--- app/models/shoot.rb
+++ app/models/shoot.rb
@@ -1,5 +1,5 @@
 class Shoot < ApplicationRecord
   validates :slug, :assignment_description, :start, :end, :location, :contact_name, :contact_phone_number, :deadline, presence: true
   validates :slug, uniqueness: true
-end
+  has_and_belongs_to_many :reporter, :photographer
 end
