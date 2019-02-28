207ae2888127675014c154caf2209888df83f45b
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-02-27 20:21:16 -0600

    fixed controller

--- app/controllers/shoots_controller.rb
+++ app/controllers/shoots_controller.rb
@@ -1,5 +1,5 @@
 class ShootsController < ApplicationController
-  def list
+  def index
     @shoots = Shoot.all
    end
 
@@ -8,43 +8,59 @@ class ShootsController < ApplicationController
    end
 
    def new
-    @shoots = Shoot.new
+    @shoot = Shoot.new
    end
 
    def create
-     if @shoot.save
-      redirect_to :action => 'list'
-   else
-      @shoot  = Shoot.all
-      render :action => 'new'
-   end
-   def shoot_params
-   params.require(:shoot).permit(:slug, :assignment_description, :start, :end, :location, :contact_name, :contact_phone_number, :deadline)
+     @shoot = Shoot.new(shoot_params)
+
+     respond_to do |format|
+       if @shoot.save
+       format.html { redirect_to @shoot,
+         notice: 'Shoot request was successfully created.' }
+        format.json { render :show, status: :created,
+        location: @shoot }
+else
+  format.html { render :new }
+  format.json { render json: @shoot.errors,
+  status: :unprocessable_entity }
+    end
+  end
 end
-   end
 
    def edit
      @shoot = Shoot.find(params[:id])
    end
 
    def update
-     @shoot = Shoot.find(params[:id])
+  respond_to do |format|
+    if @shoot.update(shoot_params)
+        format.html { redirect_to @shoot, 
+          notice: 'Shoot request was successfully updated.' }
+        format.json { render :show, status: :ok, location: @shoot }
+      else
+        format.html { render :edit } 
+        format.json { render json: @shoot.errors,
+          status: :unprocessable_entity }
+      end
+    end
+  end
 
-   if @shoot.update_attributes(shoot_param)
-      redirect_to :action => 'show', :id => @shoot
-   else
-      @shoot = Shoot.all
-      render :action => 'edit'
+   def destroy
+    @shoot.destroy
+    respond_to do |format|
+      format.html { redirect_to shoots_url,
+        notice: 'Shoot request was successfully destroyed.' }
+      format.json { head :no_content }
    end
-
 end
 
+private
+    # Use callbacks to share common setup or constraints between actions.
+def set_shoot
+@shoot = Shoot.find(params[:id])
+end
+# Never trust parameters from the scary internet, only allow the white # list through.
 def shoot_param
    params.require(:shoot).permit(:slug, :assignment_description, :start, :end, :location, :contact_name, :contact_phone_number, :deadline)
 end
-   end
-
-   def delete
-     Shoot.find(params[:id]).destroy
-   redirect_to :action => 'list'
-   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'shoot/list'
+     get 'shoot/new'
+     post 'shoot/create'
+     patch 'shoot/update'
+     get 'shoot/list'
+     get 'shoot/show'
+     get 'shoot/edit'
+     get 'shoot/delete'
+     get 'shoot/update'
 end
