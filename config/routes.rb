c8dcaa41d5dd7b39af929b31961c1887234dddf6
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-03-01 21:04:16 -0600

    made RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'shoot/list'
-     get 'shoot/new'
-     post 'shoot/create'
-     patch 'shoot/update'
-     get 'shoot/list'
-     get 'shoot/show'
-     get 'shoot/edit'
-     get 'shoot/delete'
-     get 'shoot/update'
+resources :shoots
 end
