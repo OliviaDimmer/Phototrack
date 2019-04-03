293188c38b4c9c0b8c1ea757fc48c993da413954
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-04-03 17:47:06 -0500

    added new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :shoots
 end
