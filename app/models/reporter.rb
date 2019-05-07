58add15882bce2422034e3672df3307c655fcea3
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-05-06 19:15:53 -0500

    rails destroy model photographer

--- app/models/reporter.rb
+++ app/models/reporter.rb
@@ -1,6 +1,6 @@
 class Reporter < ApplicationRecord
-  validates :name, presence: true
-  validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "XXX-XXX-XXXX" }
-  has_and_belongs_to_many :shoots
-  validates :uid, uniqueness: true
+  # validates :name, presence: true on: :update
+  # validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "XXX-XXX-XXXX" }
+  # has_and_belongs_to_many :shoots
+  # validates :uid, uniqueness: true
 end
--- test/models/reporter_test.rb
+++ test/models/reporter_test.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class ReporterTest < ActiveSupport::TestCase
   test "reporter fields must not be empty" do
-    Reporter = Reporter.new
-    assert Reporter.invalid?
-    assert Reporter.errors[:name].any?
-    assert Reporter.errors[:email].any?
-    assert Reporter.errors[:phone_number].any?
+    reporter = Reporter.new
+    reporter.save
+    assert reporter.invalid?
+    assert reporter.errors[:name].any?
+    assert reporter.errors[:email].any?
+    assert reporter.errors[:phone_number].any?
  end
 end
