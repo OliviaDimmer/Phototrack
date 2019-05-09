a533a211a3ce1ac9307062da996c7e58f4850674
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-05-08 19:08:58 -0500

    added basic nav

--- config/application.rb
+++ config/application.rb
@@ -16,3 +16,13 @@ module Phototrack
     # the framework and any gems in your application.
   end
 end
+
+module Time_zone
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = "Central Time (US & Canada)"
+  end
+end
